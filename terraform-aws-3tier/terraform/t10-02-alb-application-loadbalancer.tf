e12ff9fa0cd5b19794d56e8c23d55d22df8987ba
VIVESH <35293085+574n13y@users.noreply.github.com>
2023-12-21 14:57:35 +0530

    Update t10-02-alb-application-loadbalancer.tf

--- terraform-aws-3tier/terraform/t10-02-alb-application-loadbalancer.tf
+++ terraform-aws-3tier/terraform/t10-02-alb-application-loadbalancer.tf
@@ -1,65 +1,104 @@
-# Terraform AWS Application Load Balancer (ALB) Outputs
-output "lb_id" {
-  description = "The ID and ARN of the load balancer we created."
-  value       = module.alb.lb_id
-}
+# Terraform AWS Application Load Balancer (ALB)
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "6.10.0"
 
-output "lb_arn" {
-  description = "The ID and ARN of the load balancer we created."
-  value       = module.alb.lb_arn
-}
+  name               = "${local.name}-alb"
+  load_balancer_type = "application"
+  vpc_id             = module.vpc.vpc_id
+  /*Option-1: Give as list with specific subnets or in next line, pass all public subnets 
+  subnets = [
+    module.vpc.public_subnets[0],
+    module.vpc.public_subnets[1]
+  ]*/
+  subnets         = module.vpc.public_subnets
+  security_groups = [module.loadbalancer_sg.security_group_id]
 
-output "lb_dns_name" {
-  description = "The DNS name of the load balancer."
-  value       = module.alb.lb_dns_name
-}
+  # Listeners
+  # HTTP Listener - HTTP to HTTPS Redirect
+  http_tcp_listeners = [
+    {
+      port        = 80
+      protocol    = "HTTP"
+      action_type = "redirect"
+      redirect = {
+        port        = "443"
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
+    }
+  ]
 
-output "lb_arn_suffix" {
-  description = "ARN suffix of our load balancer - can be used with CloudWatch."
-  value       = module.alb.lb_arn_suffix
-}
+  # Target Groups
+  target_groups = [
+    # App1 Target Group - TG Index = 0
+    {
+      name_prefix          = "app1-"
+      backend_protocol     = "HTTP"
+      backend_port         = 80
+      target_type          = "instance"
+      deregistration_delay = 10
+      health_check = {
+        enabled             = true
+        interval            = 30
+        path                = "/app1/index.html"
+        port                = "traffic-port"
+        healthy_threshold   = 3
+        unhealthy_threshold = 3
+        timeout             = 6
+        protocol            = "HTTP"
+        matcher             = "200-399"
+      }
+      protocol_version = "HTTP1"
+      /* # App1 Target Group - Targets
+      targets = {
+        my_app1_vm1 = {
+          target_id = module.ec2_private_app1.id[0]
+          port      = 80
+        },
+        my_app1_vm2 = {
+          target_id = module.ec2_private_app1.id[1]
+          port      = 80
+        }
+      }
+      tags =local.common_tags # Target Group Tags*/
+    },
+  ]
 
-output "lb_zone_id" {
-  description = "The zone_id of the load balancer to assist with creating DNS records."
-  value       = module.alb.lb_zone_id
-}
+  # HTTPS Listener
+  https_listeners = [
+    # HTTPS Listener Index = 0 for HTTPS 443
+    {
+      port     = 443
+      protocol = "HTTPS"
+      #certificate_arn    = module.acm.this_acm_certificate_arn
+      certificate_arn = module.acm.acm_certificate_arn
+      action_type     = "fixed-response"
+      fixed_response = {
+        content_type = "text/plain"
+        message_body = "Fixed Static message - for Root Context"
+        status_code  = "200"
+      }
+    },
+  ]
 
-output "http_tcp_listener_arns" {
-  description = "The ARN of the TCP and HTTP load balancer listeners created."
-  value       = module.alb.http_tcp_listener_arns
-}
+  # HTTPS Listener Rules
+  https_listener_rules = [
+    # Rule-1: /app1* should go to App1 EC2 Instances
+    {
+      https_listener_index = 0
+      priority             = 1
+      actions = [
+        {
+          type               = "forward"
+          target_group_index = 0
+        }
+      ]
+      conditions = [{
+        path_patterns = ["/*"]
+      }]
+    },
+  ]
 
-output "http_tcp_listener_ids" {
-  description = "The IDs of the TCP and HTTP load balancer listeners created."
-  value       = module.alb.http_tcp_listener_ids
-}
-
-output "https_listener_arns" {
-  description = "The ARNs of the HTTPS load balancer listeners created."
-  value       = module.alb.https_listener_arns
-}
-
-output "https_listener_ids" {
-  description = "The IDs of the load balancer listeners created."
-  value       = module.alb.https_listener_ids
-}
-
-output "target_group_arns" {
-  description = "ARNs of the target groups. Useful for passing to your Auto Scaling group."
-  value       = module.alb.target_group_arns
-}
-
-output "target_group_arn_suffixes" {
-  description = "ARN suffixes of our target groups - can be used with CloudWatch."
-  value       = module.alb.target_group_arn_suffixes
-}
-
-output "target_group_names" {
-  description = "Name of the target group. Useful for passing to your CodeDeploy Deployment Group."
-  value       = module.alb.target_group_names
-}
-
-output "target_group_attachments" {
-  description = "ARNs of the target group attachment IDs."
-  value       = module.alb.target_group_attachments
+  tags = local.common_tags # ALB Tags
 }
